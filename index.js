55338c2150b86a6848742a616af5e8cc7b05c48c
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-02-13 21:09:12 +0100

    Display grid and add hover effect on each cell

---
+++ index.js
@@ -0,0 +1,19 @@
+
+(function createGrid() {
+  const gridElement = document.querySelector('#grid');
+
+  for (let i = 0; i < 16; i++) {
+    const rowElement = document.createElement('div')
+    rowElement.classList.add('row');
+    for (let i = 0; i < 16; i++) {
+      const rowCellElement = document.createElement('div')
+      rowCellElement.classList.add('row-cell');
+      rowCellElement.addEventListener('mouseleave', event => {
+        event.currentTarget.classList.add('active');
+      });
+
+      rowElement.appendChild(rowCellElement);
+    }
+    gridElement.appendChild(rowElement);
+  }
+})();
